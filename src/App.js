4f252157ddc8d7a8f0081ec0035c1c82c2196180
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2024-11-23 18:48:03 +0530

    assessment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import FirstComponent from "./components/FirstComponent";
+import SecondComponent from "./components/SecondComponent";
+import ThirdComponent from "./components/ThirdComponent";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div style={{ padding: "20px" }}>
+              <h1>Welcome to the React Assessment</h1>
+              <FirstComponent />
+              <SecondComponent />
+              <ThirdComponent />
+            </div>
+          }
+        />
+        <Route path="/first" element={<FirstComponent />} />
+        <Route path="/second" element={<SecondComponent />} />
+        <Route path="/third" element={<ThirdComponent />} />
+        <Route path="/random" element={<div style={{ padding: "20px" }}>This is a random page!</div>} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
